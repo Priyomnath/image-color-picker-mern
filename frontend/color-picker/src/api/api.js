078180233a54e5f90efcbe99325750d705309e2f
Priyomnath <nathpriyom721@gmail.com>
2026-08-02 01:28:24 +0600

    axios update

--- frontend/color-picker/src/api/api.js
+++ frontend/color-picker/src/api/api.js
@@ -1,13 +1,8 @@
 import axios from "axios";
 
-
 const api = axios.create({
-
-baseURL: "https://image-color-picker-mern.onrender.com/api",
-
-withCredentials:true
-
+  baseURL: "https://image-color-picker-mern.onrender.com/api",
+  withCredentials: true,
 });
-
 
 export default api;
